556c315a1ebc871a45ef6b72ed4b0c64e84773a0
adyashapadhy <adya0812@gmail.com>
2021-05-01 23:34:46 +0530

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,56 @@
+
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <div className="faq">
+                <h2>Any Questions <span>FAQ</span></h2>
+                <div className="question">
+                    <h4>How do I start?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum, sit amet.</p>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adisunbxh
+                            Nebjsjschd, neque. 
+                        </p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Daily Schedule</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum, sit amet.</p>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adisunbxh
+                            Nebjsjschd, neque. 
+                        </p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Different payments methods</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum, sit amet.</p>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adisunbxh
+                            Nebjsjschd, neque. 
+                        </p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>What products do you offer?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum, sit amet.</p>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adisunbxh
+                            Nebjsjschd, neque. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
+
+
+
