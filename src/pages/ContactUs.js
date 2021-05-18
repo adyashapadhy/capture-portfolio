2aba89a747eb5af33d7dd9c8e2178cd5243127a4
adyashapadhy <adya0812@gmail.com>
2021-05-18 16:00:41 +0530

    React Router

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,10 @@
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h1>Our Contact </h1>
+        </div>
+    );
+};
+
+export default ContactUs;
---
+++ src/pages/OurWork.js
@@ -0,0 +1,10 @@
+
+const OurWork = () => {
+    return (
+        <div>
+            <h1>Our Works</h1>
+        </div>
+    );
+};
+
+export default OurWork;
