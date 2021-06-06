10ac71b452a5e9201c9d462e279ba18705a6c115
adyashapadhy <adya0812@gmail.com>
2021-06-06 11:40:30 +0530

    Page Transistions

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,9 +1,20 @@
 
+//Animations 
+import {motion} from "framer-motion";
+import {pageAnimation} from "../animation";
+
+
 const ContactUs = () => {
     return (
-        <div>
+        <motion.div 
+        variants={pageAnimation} 
+        exit="exit" 
+        initial="hidden" 
+        animate="show"
+        style={{background:"#fff"}}
+        >
             <h1>Our Contact </h1>
-        </div>
+        </motion.div>
     );
 };
 
